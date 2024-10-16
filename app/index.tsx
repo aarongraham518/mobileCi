9e58a10faea665300e2dae2348048a09a4ac01bd
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2024-10-16 17:50:40 -0400

    update text

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the front of the app.</Text>
+        <Text style={styles.subtitle}>This is the front of the.</Text>
       </View>
     </View>
   );
